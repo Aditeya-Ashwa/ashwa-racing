fe749870d4164f2ee08f6171e1bba055247667a4
Aditeya-Ashwa <aditeya.ashwa@gmail.com>
2025-12-30 18:24:12 +0530

    Some sponsors changes

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -1,26 +1,35 @@
 const sponsorData = {
-    "TITLE SPONSOR": [
+    "EXECUTIVE SPONSOR": [
         "assets/images/sponsors/infineon.svg"
     ],
 
-    "GOLD PARTNERS": [
-        "assets/images/sponsors/analogdevices.svg",
-        "assets/images/sponsors/dmgmori.svg",
-        "assets/images/sponsors/huntsman.svg"
+    "PLATINUM SPONSOR": [
+        "assets/images/sponsors/rvce.svg"
     ],
 
-    "SILVER PARTNERS": [
-        "assets/images/sponsors/lapp.svg",
-        "assets/images/sponsors/lioncircuits.svg",
-        "assets/images/sponsors/pcbpower.webp"
+    "GOLD SPONSOR":[
+        "assets/images/sponsors/vrl.svg",
+    ],
+
+    "SILVER SPONSOR":[
+        "assets/images/sponsors/pegasyssystemspvtltd.svg"
+    ],
+
+    "TECHNICAL PARTNERS": [
+        
     ],
 
     "TECHNICAL PARTNERS": [
         "assets/images/sponsors/skf.svg",
         "assets/images/sponsors/uniflex.svg",
-        "assets/images/sponsors/vrl.svg",
+        "assets/images/sponsors/lapp.svg",
+        "assets/images/sponsors/lioncircuits.svg",
+        "assets/images/sponsors/pcbpower.webp",
         "assets/images/sponsors/delhivery.svg",
-        "assets/images/sponsors/motul.svg"
+        "assets/images/sponsors/motul.svg",
+        "assets/images/sponsors/analogdevices.svg",
+        "assets/images/sponsors/dmgmori.svg",
+        "assets/images/sponsors/huntsman.svg",
     ]
 };
 
