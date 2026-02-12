b4145744eeac60cc3700417b5fccec121f06614a
Aditeya-Ashwa <aditeya.ashwa@gmail.com>
2026-02-13 00:51:07 +0530

    some image optimizations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,34 @@
 const heroImages = [
-  "assets/images/team/2021 hybrd.jpg",
-  "assets/images/team/2025.jpg",
-  "assets/images/team/Consulate.jpg",
-  "assets/images/team/Hyperloop.jpg"
+  "assets/images/team/2021 hybrd.webp",
+  "assets/images/team/2025.webp",
+  "assets/images/team/consulate.webp",
+  "assets/images/team/hyperloop.webp"
 ];
 
 const heroBg = document.querySelector(".hero-bg");
 
 let currentIndex = 0;
 
-function changeHeroBackground() {
-    heroBg.classList.remove("active");
-
-    setTimeout(() => {
-        heroBg.style.backgroundImage = `url('${heroImages[currentIndex]}')`;
-        heroBg.classList.add("active");
-
-        currentIndex = (currentIndex + 1) % heroImages.length;
-    }, 600); // sync with fade-out
+// Preload images
+function preloadImages(images) {
+  images.forEach(src => {
+    const img = new Image();
+    img.src = src;
+  });
 }
 
-// Initial load
+preloadImages(heroImages);
+
+function changeHeroBackground() {
+  heroBg.classList.remove("active");
+
+  setTimeout(() => {
+    heroBg.style.backgroundImage = `url('${heroImages[currentIndex]}')`;
+    heroBg.classList.add("active");
+
+    currentIndex = (currentIndex + 1) % heroImages.length;
+  }, 1500); // slightly faster fade
+}
 changeHeroBackground();
 
-// Rotate every 6 seconds
-setInterval(changeHeroBackground, 6000);
+setInterval(changeHeroBackground, 7500);
